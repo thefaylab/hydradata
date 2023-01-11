63ea8343da73c826f5494d0dddf2956949217a49
Sarah Gaichas <sarah.gaichas@noaa.gov>
2023-01-11 10:58:01 -0500

    initial range based on quantiles + binwidths

--- data-raw/lenbintests.R
+++ data-raw/lenbintests.R
@@ -8,16 +8,32 @@ quantsurv <- realSurveyLennumcomp %>%
   filter(variable == "numbers") %>%
   group_by(Name) %>%
   summarise(lenqant = quantile(lenbin, weights=value, 
-                          c(0.05, 0.25, 0.5, 0.75, 0.95)), 
-            q = c(0.05, 0.25, 0.5, 0.75, 0.95))
+                          c(0.05, 0.10, 0.5, 0.90, 0.95)), 
+            quant = c(0.05, 0.10, 0.5, 0.90, 0.95)) %>%
+  tidyr::pivot_wider(names_from = quant, names_prefix = "q",values_from = lenqant)
+
+plotsurvlen <- realSurveyLennumcomp %>% 
+  filter(variable == "numbers") %>%
+  left_join(quantsurv) %>%
+  ggplot(aes(x=lenbin, y=value)) +
+  geom_bar(stat = "identity", width = 1) +
+  facet_wrap(~Name, scales = "free") #+ 
+  # geom_vline(data=filter(quantsurv, Name=="Atlantic cod"),
+  #            xintercept = c("q0.05", "q0.95")) +
+  # geom_vline(data=filter(quantsurv, Name=="Atlantic herring"),
+  #            xintercept = c("q0.05", "q0.95")) +
+  # geom_vline(data=filter(quantsurv, Name=="Atlantic mackerel"),
+  #            xintercept = c("q0.05", "q0.95"))
+
 
 quantfish <- realFisheryLencompRaw %>%
   filter(variable == "abundance",
          !is.na(value)) %>%
   group_by(Name) %>%
   summarise(lenqant = quantile(lenbin, weights=value, 
-                               c(0.05, 0.25, 0.5, 0.75, 0.95)), 
-            q = c(0.05, 0.25, 0.5, 0.75, 0.95))
+                               c(0.05, 0.10, 0.5, 0.90, 0.95)), 
+            quant = c(0.05, 0.10, 0.5, 0.90, 0.95)) %>%
+  tidyr::pivot_wider(names_from = quant, names_prefix = "q",values_from = lenqant)
 
 fishlen <- ggplot(realFisheryLencompRaw %>% filter(variable == "abundance"),
                   aes(x=lenbin, y=value)) +
@@ -31,3 +47,14 @@ survlen <- ggplot(realSurveyLennumcomp %>% filter(variable == "numbers"),
   geom_bar(stat = "identity", width = 1) +
   #sapply(quantsurv, function(lenquant) geom_vline(aes(xintercept = lenquant))) +
   facet_wrap(~Name, scales = "free")
+
+# use q.0.10 survey for lower limit, q.0.90 fishery for upper limit
+# divide that range into 5 equal bins, allow bin 1 to start at 0, bin 5 to extend to max
+
+Nsizebins <- 5
+
+mixquant <- quantsurv %>%
+  select(Name, q0.1) %>%
+  left_join(quantfish %>% select(Name, q0.9)) %>%
+  mutate(range = q0.9 - q0.1,
+         binwid = ceiling(range/Nsizebins))
